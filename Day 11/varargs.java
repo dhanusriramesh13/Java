aa8bbdd7eb371290eea6ad631f6847f86b3caa60
Dhanusri Ramesh <dhanusriramesh13@gmail.com>
2025-10-13 15:09:40 +0530

    Add files via upload

---
+++ Day 11/varargs.java
@@ -0,0 +1,10 @@
+package javafullproject;
+
+public class varargs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day 11/wrap.java
@@ -0,0 +1,12 @@
+package javafullproject;
+
+public class wrap {
+
+	public static void main(String[] args) {
+		int num = 1;
+				Integer num1 = num;
+		System.out.println(num1 instanceof Integer);
+
+	}
+
+}
